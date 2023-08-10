652b18ebb55ed018930f143aeba904635ba78f91
Pablo Nolasco Castro <pablo.nolasco@people-media.com.mx>
2023-08-10 18:22:17 -0500

    Feat: add consulta de folios ordenes, convenios

--- src/main/java/com/imss/sivimss/balancecaja/beans/FolioOrdenServicio.java
+++ src/main/java/com/imss/sivimss/balancecaja/beans/FolioOrdenServicio.java
@@ -1,7 +1,17 @@
 package com.imss.sivimss.balancecaja.beans;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.imss.sivimss.balancecaja.util.AppConstantes;
+import com.imss.sivimss.balancecaja.util.DatosRequest;
+import com.imss.sivimss.balancecaja.util.SelectQueryUtil;
 
 public class FolioOrdenServicio {
 
@@ -10,6 +20,7 @@ public class FolioOrdenServicio {
 	
 	private static final Logger log = LoggerFactory.getLogger(FolioOrdenServicio.class);
 
+	private String query;
 	
 	private FolioOrdenServicio() {
 		
@@ -20,5 +31,28 @@ public class FolioOrdenServicio {
 			folioOrdenServicio= new FolioOrdenServicio();
 		}
 		return folioOrdenServicio;
+	}
+	
+	public DatosRequest obtenerFolios(String folio) {
+		DatosRequest datosRequest= new DatosRequest();
+		Map<String, Object>parametros= new HashMap<>();
+		SelectQueryUtil selectQueryUtil= new SelectQueryUtil();
+		selectQueryUtil.select("DISTINCT STO.CVE_FOLIO AS folio")
+		.from("SVC_ORDEN_SERVICIO STO")
+		.innerJoin("SVT_PAGO_BITACORA SPB", "SPB.ID_REGISTRO = STO.ID_ORDEN_SERVICIO ")
+		.innerJoin("SVC_FLUJO_PAGOS SFP", "SFP.ID_FLUJO_PAGOS = SPB.ID_FLUJO_PAGOS")
+		.innerJoin("SVT_PAGO_DETALLE SPD", "SPD.ID_PAGO_BITACORA = SPB.ID_PAGO_BITACORA")
+		.innerJoin("SVC_METODO_PAGO SPA", "SPA.ID_METODO_PAGO = SPD.ID_METODO_PAGO ")
+		.where("SPB.ID_FLUJO_PAGOS =1 ")
+		.and("STO.ID_ESTATUS_ORDEN_SERVICIO IN (1,4)")
+		.and("SPD.CVE_ESTATUS IN (4,5,8)")
+		.and("STO.CVE_FOLIO LIKE '%"+folio+"%'")
+		.orderBy("STO.CVE_FOLIO ASC");
+		query=selectQueryUtil.build();
+		log.info(query);
+		String encoded=DatatypeConverter.printBase64Binary(query.getBytes(StandardCharsets.UTF_8));
+		parametros.put(AppConstantes.QUERY, encoded);
+		datosRequest.setDatos(parametros);
+		return datosRequest;
 	}
 }
--- src/main/java/com/imss/sivimss/balancecaja/service/impl/BalanceCajaServiceImpl.java
+++ src/main/java/com/imss/sivimss/balancecaja/service/impl/BalanceCajaServiceImpl.java
@@ -1,6 +1,12 @@
 package com.imss.sivimss.balancecaja.service.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.xml.bind.DatatypeConverter;
 
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
@@ -9,13 +15,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
 import com.imss.sivimss.balancecaja.beans.FolioConvenio;
 import com.imss.sivimss.balancecaja.beans.FolioOrdenServicio;
+import com.imss.sivimss.balancecaja.model.request.FolioRequest;
+import com.imss.sivimss.balancecaja.model.response.FolioResponse;
 import com.imss.sivimss.balancecaja.service.BalanceCajaService;
+import com.imss.sivimss.balancecaja.util.AppConstantes;
 import com.imss.sivimss.balancecaja.util.DatosRequest;
 import com.imss.sivimss.balancecaja.util.LogUtil;
+import com.imss.sivimss.balancecaja.util.MensajeResponseUtil;
 import com.imss.sivimss.balancecaja.util.ProviderServiceRestTemplate;
 import com.imss.sivimss.balancecaja.util.Response;
+
 
 
 @Service
@@ -46,20 +58,95 @@ public class BalanceCajaServiceImpl implements BalanceCajaService{
 
 	@Override
 	public Response<Object> consultarFolioOrden(DatosRequest request, Authentication authentication) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		FolioRequest folioRequest= new FolioRequest();
+		try {
+            logUtil.crearArchivoLog(Level.INFO.toString(), this.getClass().getSimpleName(), this.getClass().getPackage().toString(), "consultarFolioOrden", AppConstantes.CONSULTA, authentication);
+            List<FolioResponse>folioResponses;
+			Gson gson= new Gson();
+			String datosJson=request.getDatos().get(AppConstantes.DATOS).toString();
+			folioRequest=gson.fromJson(datosJson,FolioRequest.class);
+			Response<Object>response;
+			response=providerServiceRestTemplate.consumirServicio(
+					folioOrdenServicio.obtenerFolios(folioRequest.getFolio()).getDatos(), 
+					urlDominio.concat(AppConstantes.CATALOGO_CONSULTAR), 
+					authentication);
+			if (response.getCodigo()==200 && !response.getDatos().toString().contains("[]")) {
+				folioResponses= Arrays.asList(modelMapper.map(response.getDatos(), FolioResponse[].class));
+				response.setDatos(folioResponses);
+			}
+
+			return MensajeResponseUtil.mensajeConsultaResponseObject(response, AppConstantes.ERROR_CONSULTAR);
+		
+		} catch (Exception e) {
+			String consulta = folioOrdenServicio.obtenerFolios(folioRequest.getFolio()).getDatos().get(AppConstantes.QUERY).toString();
+	        String decoded = new String(DatatypeConverter.parseBase64Binary(consulta));
+	        log.error(AppConstantes.ERROR_QUERY.concat(decoded));
+	        log.error(e.getMessage());
+	        logUtil.crearArchivoLog(Level.WARNING.toString(), this.getClass().getSimpleName(), this.getClass().getPackage().toString(), AppConstantes.ERROR_LOG_QUERY + decoded, AppConstantes.CONSULTA, authentication);
+	        throw new IOException(AppConstantes.ERROR_CONSULTAR, e.getCause());
+		}
 	}
 
 	@Override
 	public Response<Object> consultarFolioConvenio(DatosRequest request, Authentication authentication) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		FolioRequest folioRequest= new FolioRequest();
+		try {
+            logUtil.crearArchivoLog(Level.INFO.toString(), this.getClass().getSimpleName(), this.getClass().getPackage().toString(), "consultarFolioOrden", AppConstantes.CONSULTA, authentication);
+            List<FolioResponse>folioResponses;
+			Gson gson= new Gson();
+			String datosJson=request.getDatos().get(AppConstantes.DATOS).toString();
+			folioRequest=gson.fromJson(datosJson,FolioRequest.class);
+			Response<Object>response;
+			response=providerServiceRestTemplate.consumirServicio(
+					folioConvenio.obtenerFoliosConvenio(folioRequest.getFolio()).getDatos(), 
+					urlDominio.concat(AppConstantes.CATALOGO_CONSULTAR), 
+					authentication);
+			if (response.getCodigo()==200 && !response.getDatos().toString().contains("[]")) {
+				folioResponses= Arrays.asList(modelMapper.map(response.getDatos(), FolioResponse[].class));
+				response.setDatos(folioResponses);
+			}
+
+			return MensajeResponseUtil.mensajeConsultaResponseObject(response, AppConstantes.ERROR_CONSULTAR);
+		
+		} catch (Exception e) {
+			String consulta = folioConvenio.obtenerFoliosConvenio(folioRequest.getFolio()).getDatos().get(AppConstantes.QUERY).toString();
+	        String decoded = new String(DatatypeConverter.parseBase64Binary(consulta));
+	        log.error(AppConstantes.ERROR_QUERY.concat(decoded));
+	        log.error(e.getMessage());
+	        logUtil.crearArchivoLog(Level.WARNING.toString(), this.getClass().getSimpleName(), this.getClass().getPackage().toString(), AppConstantes.ERROR_LOG_QUERY + decoded, AppConstantes.CONSULTA, authentication);
+	        throw new IOException(AppConstantes.ERROR_CONSULTAR, e.getCause());
+		}
 	}
 
 	@Override
 	public Response<Object> consultarFolioRenovacionConvenio(DatosRequest request, Authentication authentication)
 			throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		FolioRequest folioRequest= new FolioRequest();
+		try {
+            logUtil.crearArchivoLog(Level.INFO.toString(), this.getClass().getSimpleName(), this.getClass().getPackage().toString(), "consultarFolioOrden", AppConstantes.CONSULTA, authentication);
+            List<FolioResponse>folioResponses;
+			Gson gson= new Gson();
+			String datosJson=request.getDatos().get(AppConstantes.DATOS).toString();
+			folioRequest=gson.fromJson(datosJson,FolioRequest.class);
+			Response<Object>response;
+			response=providerServiceRestTemplate.consumirServicio(
+					folioConvenio.obtenerFoliosRenovacionConvenio(folioRequest.getFolio()).getDatos(), 
+					urlDominio.concat(AppConstantes.CATALOGO_CONSULTAR), 
+					authentication);
+			if (response.getCodigo()==200 && !response.getDatos().toString().contains("[]")) {
+				folioResponses= Arrays.asList(modelMapper.map(response.getDatos(), FolioResponse[].class));
+				response.setDatos(folioResponses);
+			}
+
+			return MensajeResponseUtil.mensajeConsultaResponseObject(response, AppConstantes.ERROR_CONSULTAR);
+		
+		} catch (Exception e) {
+			String consulta = folioConvenio.obtenerFoliosRenovacionConvenio(folioRequest.getFolio()).getDatos().get(AppConstantes.QUERY).toString();
+	        String decoded = new String(DatatypeConverter.parseBase64Binary(consulta));
+	        log.error(AppConstantes.ERROR_QUERY.concat(decoded));
+	        log.error(e.getMessage());
+	        logUtil.crearArchivoLog(Level.WARNING.toString(), this.getClass().getSimpleName(), this.getClass().getPackage().toString(), AppConstantes.ERROR_LOG_QUERY + decoded, AppConstantes.CONSULTA, authentication);
+	        throw new IOException(AppConstantes.ERROR_CONSULTAR, e.getCause());
+		}
 	}
 }
